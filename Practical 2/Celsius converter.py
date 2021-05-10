c0bca3f7481b75121124ae1b0cb11c5551894322
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-05-10 09:23:57 +0800

    Changes

--- Practical 2/Celsius converter.py
+++ Practical 2/Celsius converter.py
@@ -1,5 +1,5 @@
 x = 0.0
-while x % 1 == 0 :  # A way to verify whether it is a whole number or not 
+while 1:  # A way to verify whether it is a whole number or not 
     try:
         x = float(input("What degree (celcius) do you want to convert to Fahrenheit (Press a letter to quit the program): "))
     except ValueError:
