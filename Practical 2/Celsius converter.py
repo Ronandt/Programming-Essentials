04d1db8589a28a5949f855d1b26c4f9048bc3897
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-05-10 09:27:37 +0800

    Added comment

--- Practical 2/Celsius converter.py
+++ Practical 2/Celsius converter.py
@@ -1,10 +1,10 @@
 x = 0.0
-while 1:  # A way to verify whether it is a whole number or not 
+while 1:  # Placeholder to keep looping
     try:
         x = float(input("What degree (celcius) do you want to convert to Fahrenheit (Press a letter to quit the program): "))
+        print(x * (9/5) + 32)  #  Formula for converting celcius to Fahrenheit
     except ValueError:
         break
-    print(x * (9/5) + 32)  #  Formula for converting celcius to Fahrenheit
 print("Goodbye!")
 
 
