670b21b501d35f83cba56b63dab616e17a055985
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-05-10 10:36:52 +0800

    added name and admin for practical 1

--- Practical 1/InfoDisplay.py
+++ Practical 1/InfoDisplay.py
@@ -1,5 +1,7 @@
+name = input("Enter name: ")
+admin = input("Enter admin number: ")
 age = int(input("Enter age: "))
 gender = input("Enter gender (Male / Female): ")
 weight = int(input("Enter weight (kg): "))
 height = int(input("Enter height (m): "))
-print(f"Hello! Declan Kuok\nYour admin no is 172397M and age is {age}\nYour gender is {gender} and bmi is {weight/height**2}")
+print(f"Hello! {name}\nYour admin no is {admin} and age is {age}\nYour gender is {gender} and bmi is {weight/height**2}")
