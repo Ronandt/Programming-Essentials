d0e31c5cb386b095f7bf1960b0507e2f85fb3a66
Isaac <49837565+Ronandt@users.noreply.github.com>
2021-05-11 08:14:21 +0800

    changes to the strucutre and some improvements

--- Practical 7/Paper scissor stone.py
+++ Practical 7/Paper scissor stone.py
@@ -1,17 +1,11 @@
 import random
-def rock_paper_scissors():
-    while 1:
-        try:
-            best_of = int(input("What format do you want to play with (Best of 3 or 5): "))
-            break
-        except ValueError:
-            print("Try again! 3 or 5")
+def rock_paper_scissors(best_of):
     cpu_score = 0
     user_score = 0
     winner = [["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]]
     while cpu_score != best_of or user_score != best_of:
         rps_user = input("Please input what you want (rock, paper, scissors): ").lower().strip()
-        random_index = random.randint(0, 2)
+        random_index = random.randint(0, 2) # Random integer from 0 to 2 for random index choice
         rps_list = ["rock", "paper", "scissors"][random_index]
         if rps_list == rps_user:
             print("Tie")
@@ -25,6 +19,12 @@ def rock_paper_scissors():
             return "CPU won"
         elif user_score == best_of:
             return "User won"
-
-
-print(rock_paper_scissors())
+best = int(input("What format do you want to play with (Best of 3 or 5): "))
+while 1:
+    try:
+        if best % 2 != 0:
+            rock_paper_scissors(best) # I know the requirements are strictly 3 and 5 but why not odd?
+            break
+        print("Input again!")
+    except ValueError:
+        print("Try again! 3 or 5")
